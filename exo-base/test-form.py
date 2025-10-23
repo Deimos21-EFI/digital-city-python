ac2b7b4b5ebb5ba7fde4377e0e975f1c17031548
EFI <efi.swing21@gmail.com>
2025-10-23 16:39:52 +0200

    exercices du jeudi

--- exo-base/test-form.py
+++ exo-base/test-form.py
@@ -5,10 +5,18 @@ from tkinter import messagebox
 def show_message():
     messagebox.showinfo("Information", "Hello, John! This is an information message.")
 
+def click():
+    messagebox.showwarning("Warning", "Warning: You have not clicked the button yet.")
+
 window = tk.Tk()
 window.title("Message Box Example")
 window.geometry("300x200")
 button = tk.Button(window, text="Show Message", command=show_message)
-button.pack()
+button2 = tk.Button(window, text="Click", command=click)
+label = tk.Label(window, text="Hello, World!")
 
+
+button.pack()
+button2.pack()
+label.pack()
 window.mainloop()
